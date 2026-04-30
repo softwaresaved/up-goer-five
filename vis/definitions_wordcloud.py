1a5c2c27fd0f2824fb53272bae4359b7ae5eb7f2
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2026-04-30 14:55:17 +0100

    Update word cloud code to process definitions from YAML

--- vis/definitions_wordcloud.py
+++ vis/definitions_wordcloud.py
@@ -5,41 +5,39 @@
 # Image from:
 #     https://society-rse.org/trademark-and-logo-policy/
 
+# Requires libraries for the imports below, notably PIL, yaml and wordcloud
+
 from os import path
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+import yaml
+
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
-# Read the whole text.
-# Replace this with the definitions pulled from the repo
-text = """
-What is Lorem Ipsum?
-
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-Why do we use it?
-
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-Where does it come from?
-
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-Where can I get some?
-
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-"""
-
 
 def parse_all_definitions():
     """Get all word definitions from the YAML defintions."""
-    pass
+    with open("dummy_yaml/dummy_definitions.yaml", "r") as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+
+    # Print the values as a dictionary
+    print("All YAML data is:", data)
+    data = data
+    all_defs = []
+    for item in data:
+        if "definition" in item:
+            all_defs.append(item["definition"])
+
+    print("All definitions pulled from the YAML are:", all_defs)
+
+    return all_defs
+
 
 def generate_wordcloud(text):
     """Generates a word cloud of input text."""
@@ -71,5 +69,9 @@ def generate_wordcloud(text):
 
 
 if __name__ == "__main__":
-    definitions = text  # TODO replace with parse_all_definitions()
-    generate_wordcloud(definitions)
+    definitions = parse_all_definitions()
+
+    # Convert list of definitions to a merged string
+    defs_combined = " ".join(definitions)
+
+    generate_wordcloud(defs_combined)
